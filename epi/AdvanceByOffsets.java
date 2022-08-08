6380f0ba4cfc709c60a808145641d1cfb6a5aaa1
meghaksh4footprints@gmail.com <meghaksh4footprints@gmail.com>
2022-08-07 21:31:51 -0400

    implemented AdvanceByOffsets problem

--- epi/AdvanceByOffsets.java
+++ epi/AdvanceByOffsets.java
@@ -7,7 +7,12 @@ public class AdvanceByOffsets {
   @EpiTest(testDataFile = "advance_by_offsets.tsv")
   public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
     // TODO - you fill in here.
-    return true;
+	int furthest = 0; int end = maxAdvanceSteps.size()-1;
+	for(int i=0;i<=furthest&&furthest<end;i++) {
+		furthest = Math.max(furthest, i+maxAdvanceSteps.get(i));
+	}
+	//System.out.println(furthest);
+    return furthest>=end;
   }
 
   public static void main(String[] args) {
