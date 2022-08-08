dfd9f6179042da27d2152043c4946d45e34f2be4
meghaksh4footprints@gmail.com <meghaksh4footprints@gmail.com>
2022-08-07 22:05:39 -0400

    implemented BuyAndSellStock problem with O(n^2) and O(n) complexities

--- epi/BuyAndSellStock.java
+++ epi/BuyAndSellStock.java
@@ -7,7 +7,33 @@ public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
     // TODO - you fill in here.
-    return 0.0;
+	double minBuy = prices.get(0);
+	double maxProfit = 0;
+	for(int i=1;i<prices.size();i++) {
+		maxProfit = Math.max(maxProfit, prices.get(i)-minBuy);
+		if(prices.get(i)<minBuy) {
+			minBuy = prices.get(i);
+		}
+	}
+	
+	return maxProfit;
+	  
+	  
+	  
+//	double globalMax = 0;
+//	double maxForI = 0;
+//	for(int i=0;i<prices.size();i++) {
+//		maxForI = 0;
+//		for(int j=i+1;j<prices.size();j++) {
+//			if(prices.get(j)-prices.get(i)>maxForI) {
+//				maxForI=prices.get(j)-prices.get(i);
+//			}
+//		}
+//		if(maxForI>globalMax) {
+//			globalMax = maxForI;
+//		}
+//	}
+//    return globalMax;
   }
 
   public static void main(String[] args) {
