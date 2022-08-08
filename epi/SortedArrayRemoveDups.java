b372395c86e2e6e6751b2c071e09cbf80253b6a7
meghaksh4footprints@gmail.com <meghaksh4footprints@gmail.com>
2022-08-07 21:45:49 -0400

    implemented remove duplicates O(n) space complexity

--- epi/SortedArrayRemoveDups.java
+++ epi/SortedArrayRemoveDups.java
@@ -1,14 +1,32 @@
 package epi;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
-
-import java.util.List;
 public class SortedArrayRemoveDups {
   // Returns the number of valid entries after deletion.
   public static int deleteDuplicates(List<Integer> A) {
     // TODO - you fill in here.
-    return 0;
+	Map<Integer, Integer> map = new HashMap<>();
+	List<Integer> l = new ArrayList<>();
+	for(int i=0;i<A.size();i++) {
+		if(map.containsKey(A.get(i))) {
+			continue;
+		}else {
+			map.put(A.get(i),0);
+			l.add(A.get(i));
+		}
+	}
+	Collections.sort(l);
+	Collections.copy(A, l);
+//	System.out.println(map);
+//	System.out.println(map.size());
+    return map.size();
   }
   @EpiTest(testDataFile = "sorted_array_remove_dups.tsv")
   public static List<Integer> deleteDuplicatesWrapper(TimedExecutor executor,
